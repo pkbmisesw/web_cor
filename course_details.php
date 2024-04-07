5763c6551d79f58df08b9f1e431798150d7b3609
AltheaIX <althea.ixy@gmail.com>
2024-04-07 14:02:18 +0700

    Fix bug

--- course_details.php
+++ course_details.php
@@ -89,12 +89,13 @@ include 'navbar.php';
 <main>
 
     <!-- course-details-area -->
-    <?php if(((!$_SESSION['level_id'] == 3) && !$_SESSION['status_aktif'] == 0)) ?>
+    <?php if(!$_SESSION['status_aktif'] == 0){ ?>
     <div class="container">
         <div class="alert alert-danger" role="alert">
             Akun anda perlu diaktivasi oleh admin, Mohon hubungi nomor WA atau <a href="https://wa.me/<?php echo urlencode($row_setting['wa']) . "?text=Saya ingin melakukan aktivasi akun." ?>"><b>Klik Disini</b></a>
         </div>
     </div>
+    <?php } ?>
     <section class="c-details-area pt-120 pb-50 wow fadeInUp" data-wow-duration=".8s" data-wow-delay=".2s">
         <div class="container">
             <div class="row">
@@ -194,7 +195,7 @@ include 'navbar.php';
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-12">
-                    <?php if(!empty($_SESSION['email']) && ((!$_SESSION['level_id'] == 3) && !$_SESSION['status_aktif'] == 0)){
+                    <?php if(!empty($_SESSION['email']) && (!$_SESSION['status_aktif'] == 0)){
                         ?>
                         <div class="c-details-sidebar mb-25" style="padding: 15px 15px 0px 15px;">
                             <div class="cd-information">
@@ -218,7 +219,7 @@ include 'navbar.php';
                             <div class="cd-video-price">
                                 <h3 class="pricing-video text-center mb-15"><?php echo "Rp. " . number_format($data_course['harga'], 0, null, '.').",-"; ?></h3>
                                 <div class="cd-pricing-btn text-center mb-30">
-                                    <?php if((!$_SESSION['level_id'] == 3) && !$_SESSION['status_aktif'] == 0){ ?>
+                                    <?php if(!$_SESSION['status_aktif'] == 0){ ?>
                                     <form id="enroll" method="POST">
                                         <input name="enroll" hidden />
                                         <?php echo (!(empty($_SESSION['email']))) ? '<a class="tp-vp-btn-green" href="javascript:{}" onclick="handleEnroll()">Enroll Now</a>' : '<a class="tp-vp-btn-green" href="logina">Enroll Now</a>' ?>
